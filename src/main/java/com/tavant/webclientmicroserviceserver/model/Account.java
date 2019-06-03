f2c8d8b53c46a857e08c0c3ecea37c27da732471
anandsharma <anandsharma.nitdgp@gmail.com>
2019-06-03 17:18:46 +0530

    account claas modified

--- src/main/java/com/tavant/webclientmicroserviceserver/model/Account.java
+++ src/main/java/com/tavant/webclientmicroserviceserver/model/Account.java
@@ -1,6 +1,7 @@
 package com.tavant.webclientmicroserviceserver.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Account implements Serializable {
 
@@ -29,6 +30,21 @@ public class Account implements Serializable {
     @Override
     public String toString() {
         return "Account [amount=" + amount + ", number=" + number + ", name=" + name + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(amount, account.amount) &&
+                Objects.equals(number, account.number) &&
+                Objects.equals(name, account.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, number, name);
     }
 }
 
